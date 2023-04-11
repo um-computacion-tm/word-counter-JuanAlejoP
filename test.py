50534dc318d7abbdd458c9b591f5497845fcbdd6
JuanAlejoP <j.patino@alumno.um.edu.ar>
2023-04-11 09:03:51 -0300

    Extender tests

--- test.py
+++ test.py
@@ -1,8 +1,24 @@
 import unittest
 from wordcounter import wordcounter
 
-class TestCountLetters(unittest.TestCase):
+class TestWordCounter(unittest.TestCase):
+    
+    def test_empty(self):
+        result = wordcounter('')
+        self.assertEqual(result, 0)
+        
+    def test_1word(self):
+        result = wordcounter('Hola')
+        self.assertEqual(result, 1)
+        
+    def test_2words(self):
+        result = wordcounter('Hola mundo')
+        self.assertEqual(result, 2)
+        
+    def test_wordsAndPunctuation(self):
+        result = wordcounter('¡Hola, mundo!')
+        self.assertEqual(result, 2)
 
-    def test_simple(self):
-        result = wordcounter('Hola mundo.')
-        self.assertEqual(result, {'hola': 1})
+
+if __name__ == '__main__':
+    unittest.main()
